5967c2002932396761c9e9c41bc6c427b1df2357
Zelai <iosueder@gmail.com>
2014-01-06 22:31:17 +0100

    casi nada

--- ZelaiBot/ZelaiBot.py
+++ ZelaiBot/ZelaiBot.py
@@ -9,11 +9,19 @@ class ZelaiBot(interface.Bot):
 	"""Bot ZelaiBot 0.1"""
 	NAME = "ZelaiBot 0.1"""
 
+
+	distancias = {}
+
 	def __init__(self, init_state):
 		"""Inicializar el bot: llamado al comienzo del juego."""
 		# llamada a super para inicializar las variables (self.)
 		#  player_num, player_count, position, map, lighthouses
 		interface.Bot.__init__(self,init_state)	
+
+		distanciaMax = max(len(self.map),len(self.map[1])) + 1
+		
+		mapaAux = self.map[:]	
+		
 
 	def play(self, state):
 		"""Jugar: llamado cada turno.
