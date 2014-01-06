665ff83aa4b331f9a3c86477bc484c8e5126a3cd
Zelai <iosueder@gmail.com>
2014-01-06 13:43:47 +0100

    Añadido init de la interface

--- ZelaiBot/ZelaiBot.py
+++ ZelaiBot/ZelaiBot.py
@@ -9,10 +9,18 @@ class ZelaiBot(interface.Bot):
 	"""Bot ZelaiBot 0.1"""
 	NAME = "ZelaiBot 0.1"""
 
+	def __init__(self, init_state):
+		"""Inicializar el bot: llamado al comienzo del juego."""
+		# llamada a super para inicializar las variables (self.)
+		#  player_num, player_count, position, map, lighthouses
+		interface.Bot.__init__(self,init_state)	
+
 	def play(self, state):
 		"""Jugar: llamado cada turno.
 		Debe dev olver una accion (jugada)."""
+		self.log("hola " + str(self.player_num)) 
 		return self.move(*(-1,0))	
+
 if __name__ == "__main__":
 	iface = interface.Interface(ZelaiBot)
 	iface.run()
