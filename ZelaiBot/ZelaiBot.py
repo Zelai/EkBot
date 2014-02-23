9fbc796bf107b26a6e36fdaf59ab31ff4b3ab633
Zelai <iosueder@gmail.com>
2014-02-23 20:34:22 +0100

    pongo d variable debug y empiezo algo doc variables

--- ZelaiBot/ZelaiBot.py
+++ ZelaiBot/ZelaiBot.py
@@ -46,6 +46,8 @@ def calculaDistancias(lighthouses, mapa):
     Esta función devuelve un diccionario con la distancia
     a cada uno de los faros
     """
+    d=0
+    if d>0: logFun("Entro en calculaDistancias","")
     distancia={}
     xMax = len(mapa[1]);
     yMax = len(mapa);
@@ -60,8 +62,9 @@ def calculaDistancias(lighthouses, mapa):
 
     for faro in range(len(lighthouses)):
         distancia[faro] = calculaDistancia(lighthouses[faro],distancia[faro])
-        logFun("d","Distancia["+str(faro)+"] + \n" + str(distancia[faro]))
+        if d>1: logFun("d","Distancia["+str(faro)+"] + \n" + str(distancia[faro]))
 
+    if d>0: logFun("Salgo de calculaDistancias","")
     return distancia
 
 class ZelaiBot(interface.Bot):
@@ -74,61 +77,80 @@ class ZelaiBot(interface.Bot):
     def __init__(self, init_state):
         """Inicializar el bot: llamado al comienzo del juego.
 
-        Llama al init de interface para actualizar algunas variables
-
-        Llama a calcular las distancias
+|        Llama al init de interface para actualizar algunas variables
+|            self.player_num
+|            self.player_count
+|            self.init_pos
+|            self.map
+|            self.lighthouses
+|        Llama a calcular las distancias
 
         :meth: interface.Bot.__init__
-
         """
         # llamada a super para inicializar las variables (self.)
         #  player_num, player_count, position, map, lighthouses
+        d=0
+        if d>0: self.log("Entro en __init__")
         interface.Bot.__init__(self,init_state) 
 
         #self.log(str(self.map))
-        self.log("lighthouses:\n" + str(self.lighthouses));
+        if d>1: self.log("lighthouses:\n" + str(self.lighthouses));
         #self.log("len(lighthouses)=%d\n" %  (len(self.lighthouses)) );
         
         # posiciones validas map[0][0] - map[17,20]
         # modo de acceso map[y][x]
-        self.distancias = calculaDistancias(self.lighthouses,self.map)
+        self.distancias = calculaDistancias(self.lighthouses,self.map) 
         #self.log("len(lighthouses)="+ str(len(self.lighthouses)) + "\n" +str(self.distancias[0]))
         
         self.estado = 0
         self.destino = 0
         
+        if d>0: self.log("Salgo en __init__")
 
     def buscafaro(self):
-        self.log("buscafaro "+str(self.position))
+        """
+            Busca el raro más cercano
+            devuelve un entero.
+        """
+        d=0
+        if d>0: self.log("Entro en buscafaro")
+        if d>1:self.log("buscafaro "+str(self.position))
         x, y = self.position
         superior = len(self.map) + len(self.map[1])
-        self.log("superior=" + str(superior))
+        if d>1: self.log("superior=" + str(superior))
         distancia_minimo = superior
         minimo = -1
         for faro in xrange(len(self.distancias)):
             if self.distancias[faro][y][x] < distancia_minimo:
                 distancia_minimo =self.distancias[faro][y][x]
                 minimo = faro
-            self.log("Bucle buscafaro: x= " + str(x) + " y= "+str(y) + " faro="+ str(faro) + " d= " + str(self.distancias[faro][y][x]) + " min=" + str(minimo) + " dmin=" + str(distancia_minimo)) 
+            if d>2: self.log("Bucle buscafaro: x= " + str(x) + " y= "+str(y) + " faro="+ str(faro) + " d= " + str(self.distancias[faro][y][x]) + " min=" + str(minimo) + " dmin=" + str(distancia_minimo)) 
+        if d>0: self.log("Salgo de buscafaro")
         return minimo 
 
     def reduce(self):
-        self.log("XXXXX reduce XXXXX")
+        """
+            Devuelve el movimiento que deberíamos hacer para ir al faro 
+            marcado en self.objetivo
+        """
+        d=0
+        if d>0: self.log("entro en reduce")
         x, y = self.position
-        self.log("posX:" + str(x) + " posY:" + str(y) + " objetivo=" + str(self.objetivo) + " d=("+str(self.lighthouses[self.objetivo][0]) + "," + str(self.lighthouses[self.objetivo][1]) +")")
+        if d>1: self.log("posX:" + str(x) + " posY:" + str(y) + " objetivo=" + str(self.objetivo) + " d=("+str(self.lighthouses[self.objetivo][0]) + "," + str(self.lighthouses[self.objetivo][1]) +")")
         tDist = self.distancias[self.objetivo]
         distanciaMin = tDist[y][x] 
         xMin =0
         yMin = 0
         for xAux in [-1,0,1]:
             for yAux in[-1,0,1]:
-                self.log("tDist[y + yAux][x+ xAux] = "+ str(tDist[y + yAux][x+ xAux]))
+                if d>2: self.log("tDist[y + yAux][x+ xAux] = "+ str(tDist[y + yAux][x+ xAux]))
                 if tDist[y + yAux][x+ xAux] < distanciaMin and tDist[y + yAux][x+xAux] <> -1:
-                    self.log("tDist[y + yAux][x+ xAux] = "+ str(tDist[y + yAux][x+ xAux]) + "("+str(yAux)+ ","+str(xAux) + ")")
+                    if d>2: self.log("tDist[y + yAux][x+ xAux] = "+ str(tDist[y + yAux][x+ xAux]) + "("+str(yAux)+ ","+str(xAux) + ")")
                     xMin = xAux
                     yMin = yAux
                     distanciaMin =tDist[y + yAux][x+ xAux]
-        self.log("voy a llegar " + str(xMin) + " " + str(yMin))
+        if d>0: self.log("Salgo en reduce")
+        if d>1: self.log("devuelvo (xMin=" + str(xMin) + ", yMin=" + str(yMin) + ")")
         return (xMin,yMin)
 
                 
@@ -138,12 +160,20 @@ class ZelaiBot(interface.Bot):
         Este es el método que hay que sobreescribir
         de interface.py"""
         self.position = state['position']
+        self.state = state
+        cx, cy = state['position']
+        lighthouses = dict((tuple(lh["position"]), lh)
+                            for lh in state["lighthouses"])
 
         if self.estado == 0:
             self.objetivo = self.buscafaro()
             self.estado = 1
         if self.estado == 1:
             xDest,yDest = self.reduce()
+            if xDest == 0 and yDest ==0:
+                self.log("estoy en destino XXXX")
+                if lighthouses[(cx,cy)]["owner"] <> self.player_num:
+                    return self.attack(state["energy"])
             return self.move(*(xDest,yDest))
             
         return self.move(*(-1,0))   
